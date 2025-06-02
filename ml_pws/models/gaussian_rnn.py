791ebacea8ee6361d515415c2dcee776a9ef6caa
Manuel Reinhardt <reinhardt@amolf.nl>
2025-06-02 13:42:42 +0200

    delete unused code

--- ml_pws/models/gaussian_rnn.py
+++ ml_pws/models/gaussian_rnn.py
@@ -5,16 +5,18 @@ import lightning as L
 
 
 class GaussianRNN(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, num_layers: int = 1):
+    def __init__(self, features: int, hidden_size: int, num_layers: int = 1):
         super(GaussianRNN, self).__init__()
         self.hidden_dim = hidden_size
         self.num_layers = num_layers
 
         # RNN layer
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.rnn = nn.RNN(features, hidden_size, num_layers, batch_first=True)
 
         # Linear layer to output mean and log-variance
-        self.output_layer = nn.Linear(hidden_size, 2)  # 2 for mean and log-variance
+        self.output_layer = nn.Linear(
+            hidden_size, 2 * features
+        )  # 2 for mean and log-variance
 
     def forward(self, x: torch.Tensor):
         batch_size, seq_len = x.size()
@@ -40,17 +42,28 @@ class GaussianRNN(nn.Module):
 
 
 class ConditionalGaussianRNN(GaussianRNN):
-    def forward(self, s: torch.Tensor, x: torch.Tensor):
-        batch_size, seq_len = x.size()
+    def __init__(
+        self,
+        input_features: int,
+        output_features: int,
+        hidden_size: int,
+        num_layers: int = 1,
+    ):
+        super().__init__(input_features + output_features, hidden_size, num_layers)
+        self.input_features = input_features
+        self.output_features = output_features
 
+    def forward(self, s: torch.Tensor, x: torch.Tensor):
         s = s.unsqueeze(-1)
 
         # shift right
         x = x.roll(1, 1).unsqueeze(-1)  # (batch_size, seq_len, 1)
         x[:, 0, :] = 0.0
 
+        input_data = torch.cat([s, x], dim=2)
+
         # Forward through RNN
-        rnn_out, hidden = self.rnn(torch.cat([s, x], dim=2))
+        rnn_out, hidden = self.rnn(input_data)
 
         # Output mean and log-variance
         output = self.output_layer(rnn_out)  # (batch_size, seq_len, 2)
@@ -64,17 +77,6 @@ class ConditionalGaussianRNN(GaussianRNN):
         log_prob = -0.5 * (torch.log(2 * torch.pi * var) + (x - mean) ** 2 / var)
         return log_prob
 
-class NeuralAutoregressiveFlow(nn.Module):
-    def __init__(self, features, hidden_features, context_features, num_layers=2):
-        self.context_net = nn.RNN(context_features, hidden_features, 1, batch_first=True)
-        self.autoregressive_transform = nn.RNN(hidden_features, hidden_features, num_layers, batch_first=True)
-
-    def forward(self, x, context):
-
-        out, _ = self.context_net(context)
-        x, _ = self.autoregressive_transform()
-
-        pass
 
 class MLPWSEstimator(L.LightningModule):
     def __init__(self, input_model, forward_model, backward_model):
@@ -86,11 +88,11 @@ class MLPWSEstimator(L.LightningModule):
         # This is not possible with automatic optimization.
         self.automatic_optimization = False
 
-    def elbo(self, s, x):
+    def elbo(self, s: torch.tensor, x: torch.tensor):
         logq_s = self.backward_model(s, x)
         logp_s = self.input_model(s)
         logp_x_given_s = self.forward_model(s, x)
-        return torch.mean(logp_s + logp_x_given_s - logq_s)
+        return logp_s + logp_x_given_s - logq_s
 
     def training_step(self, batch, batch_idx):
         s, x = batch
@@ -100,15 +102,15 @@ class MLPWSEstimator(L.LightningModule):
         # forward step
         opt_forward.zero_grad()
         forward_loss = -self.forward_model(s, x)
-        self.log('forward_loss', forward_loss)
+        self.log("forward_loss", forward_loss)
         self.manual_backward(forward_loss)
         opt_forward.step()
 
         # backward step
         opt_backward.zero_grad()
         preds = self.backward_model.rsample(x)
-        backward_loss = self.elbo(preds, x)
-        self.log('backward_loss', backward_loss)
+        backward_loss = self.elbo(preds, x).mean()
+        self.log("backward_loss", backward_loss)
         self.manual_backward(backward_loss)
         opt_backward.step()
 
@@ -118,13 +120,12 @@ class MLPWSEstimator(L.LightningModule):
         return forward_opt, backward_opt
 
 
-
 class DoeEstimator(L.LightningModule):
     def __init__(self, input_dim, hidden_dim, num_layers=1, learning_rate=1e-3):
         super().__init__()
         self.lr = learning_rate
         self.conditional_model = ConditionalGaussianRNN(
-            2 * input_dim, hidden_dim, num_layers
+            input_dim, input_dim, hidden_dim, num_layers
         )
         self.marginal_model = GaussianRNN(input_dim, hidden_dim, num_layers)
         self.save_hyperparameters()
@@ -137,12 +138,12 @@ class DoeEstimator(L.LightningModule):
         # conditional model
         mean, log_var = self.conditional_model(s, x)
         cond_loss = criterion(mean, x, log_var.exp())
-        self.log('cond_loss', cond_loss)
+        self.log("cond_loss", cond_loss)
 
         # marginal model
         mean, log_var = self.marginal_model(x)
         marg_loss = criterion(mean, x, log_var.exp())
-        self.log('marg_loss', marg_loss)
+        self.log("marg_loss", marg_loss)
 
         loss = cond_loss + marg_loss
 
